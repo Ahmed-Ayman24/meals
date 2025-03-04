afe6baf76659f41953e3ca9608997f89999221b1
Ahmed <ahmed.ayman19.aa@gmail.com>
2025-03-04 16:50:58 +0200

    ready

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'category/:categoryName',
+    renderMode: RenderMode.Client
+    },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
