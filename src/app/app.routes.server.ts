33a98c015b1a307e58a2b14143c920a56fe3c969
Ahmed <ahmed.ayman19.aa@gmail.com>
2025-03-04 17:14:13 +0200

    ready

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client
     },
   {
+    path: 'mealdetails/:mealId',
+    renderMode: RenderMode.Client
+    },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
